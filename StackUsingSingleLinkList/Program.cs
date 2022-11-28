8708ebeb41d6c4bf7965f91937b3bbffc8a18ec1
overol1221 <fiqiachmad12@gmail.com>
2022-11-28 16:52:33 +0700

    Create case 1 (Enter number)

--- StackUsingSingleLinkList/Program.cs
+++ StackUsingSingleLinkList/Program.cs
@@ -73,8 +73,16 @@ namespace StackUsingSingleLinkList
                 Console.WriteLine("3. Display");
                 Console.WriteLine("4. Exit");
                 Console.Write("\nEnter your choice: ");
-                
-                
+                string Input = Console.ReadLine();
+                char ch = Convert.ToChar(Input == "" ? "0" : Input);
+                switch (ch)
+                {
+                    case '1':
+                        Console.Write("\nEnter a number :");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        s.push(num);
+                        break;
+                }
 
 
             }
