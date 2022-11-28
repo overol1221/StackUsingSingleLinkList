d0d36b03eff3a86aa856ad7d3a402e4ffcb7acde
overol1221 <fiqiachmad12@gmail.com>
2022-11-28 16:26:06 +0700

    Create class node

--- StackUsingSingleLinkList/Program.cs
+++ StackUsingSingleLinkList/Program.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace StackUsingSingleLinkList
 {
+    class node
+    {
+        public int info;
+        public node next;
+        public node(int i, node n)
+        {
+            info = i;
+            next = n;
+        }
+    }
 
+   
     class Program
     {
         static void Main(string[] args)
