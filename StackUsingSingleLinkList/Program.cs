197dd0cf40d0b1021e3bbcc70c1eabf3bd178ee4
overol1221 <fiqiachmad12@gmail.com>
2022-11-28 16:37:54 +0700

    Make a pop info after next node

--- StackUsingSingleLinkList/Program.cs
+++ StackUsingSingleLinkList/Program.cs
@@ -42,6 +42,13 @@ namespace StackUsingSingleLinkList
             Console.WriteLine("\n" + element + "pushed");
         }
 
+        public void pop()
+        {
+            Console.WriteLine("\n The poped elements is: " + top.info);
+            top = top.next; // make top point to the next node is squence
+        }
+
+
         
     }
     class Program
